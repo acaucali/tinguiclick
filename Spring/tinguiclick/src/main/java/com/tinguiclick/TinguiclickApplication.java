65b829176cf3c128bde40de3985065f5267cb21f
acaucali <anrrecai@gmail.com>
2021-05-23 19:31:31 -0500

    Spring Base
    Spring base proyecto de desarrollo

---
+++ Spring/tinguiclick/src/main/java/com/tinguiclick/TinguiclickApplication.java
@@ -0,0 +1,13 @@
+package com.tinguiclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinguiclickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinguiclickApplication.class, args);
+	}
+
+}
---
+++ Spring/tinguiclick/src/test/java/com/tinguiclick/TinguiclickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinguiclick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinguiclickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
