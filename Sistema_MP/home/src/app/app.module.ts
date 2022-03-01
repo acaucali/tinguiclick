e18312960163f64bcfb50db68c8b189be6de71ec
EstebanOG <51457721+EstebanOG@users.noreply.github.com>
2022-03-01 08:18:06 -0500

    Creación de proyecto Home

---
+++ Sistema_MP/home/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { HeaderMobileComponent } from './modules/header-mobile/header-mobile.component';
+import { NewletterComponent } from './modules/newletter/newletter.component';
+import { FooterComponent } from './modules/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeaderMobileComponent,
+    NewletterComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Sistema_MP/home/src/app/modules/footer/footer.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { Path } from '../../config';
+
+declare var jQuery:any;
+declare var $:any;
+
+import { CategoriesService } from 'src/app/services/categories.service';
+import { SubCategoriesService } from 'src/app/services/sub-categories.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  path:String = Path.url;
+  categories:any[];
+  render:Boolean = true;
+  categoriesList:Array<any> = [];
+
+  constructor(private categoriesService: CategoriesService,
+    private subCategoriesService:SubCategoriesService) { }
+
+  ngOnInit(): void {
+
+    /*=====================================
+    Tomamos la data de las categorías
+    ======================================*/
+
+    this.categoriesService.getData()
+    .subscribe(resp =>{
+      this.categories = resp as any[];
+
+      /*=====================================
+      Recorrido por el objeto de la data de categorias
+      ======================================*/
+
+      let i;
+      for(i in resp){
+        /*=====================================
+        Separamos los nombres de categorias
+        ======================================*/
+        this.categoriesList.push(resp[i].name)
+      }
+    })
+  }
+
+  /*=====================================
+  Función que nos avisa cuando finaliza el renderizado de Angular
+  ======================================*/
+  callback(){
+    if(this.render){
+      this.render = false;
+      let arraySubCategories = [];
+      /*=====================================
+      Separar las categorias
+      ======================================*/
+      this.categoriesList.forEach(category =>{
+
+        /*=====================================
+        Tomando la colección de las sub-categorías filtrando con los nombres de categoría
+        ======================================*/
+        this.subCategoriesService.getFilterData("category", category)
+        .subscribe(resp=>{
+
+          /*=====================================
+          Hacemos un recorrido por la colección general de subcategorias y clasificamos
+          de acuerdo a la categoría que correspondan
+          ======================================*/
+
+          let i;
+          for(i in resp){
+            arraySubCategories.push(
+              {
+                "category": resp[i].category,
+                "subcategory": resp[i].name,
+                "url": resp[i].url
+              }
+            )
+          }
+
+          /*=====================================
+          Recorremos el array de objetos nuevo para buscar coincidencias con las listas de categorías
+          ======================================*/
+          for(i in arraySubCategories){
+            if(category == arraySubCategories[i].category){
+
+              $(`[category-footer='${category}']`).after(
+              `<a href="products/${arraySubCategories[i].url}">${arraySubCategories[i].subcategory}</a>
+              `)
+            }
+          }
+
+        })
+
+      })
+    }
+  }
+}
