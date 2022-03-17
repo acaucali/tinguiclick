3242cf9a0ee09816310cef96f3aa5a4991c21973
EstebanOG <51457721+EstebanOG@users.noreply.github.com>
2022-03-16 22:15:39 -0500

    Se organiza filtrado de subcategorias para la vitrina del home

--- Sistema_MP/home/src/app/page/home/home-showcase/home-showcase.component.ts
+++ Sistema_MP/home/src/app/page/home/home-showcase/home-showcase.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Path } from '../../../config';
+
+declare var jQuery:any;
+declare var $:any;
+
 import { CategoriesService } from 'src/app/services/categories.service';
+import { SubCategoriesService } from 'src/app/services/sub-categories.service';
+
 @Component({
   selector: 'app-home-showcase',
   templateUrl: './home-showcase.component.html',
@@ -10,8 +16,10 @@ export class HomeShowcaseComponent implements OnInit {
   path: String = Path.url;
   categories:Array<any> = [];
   cargando:Boolean = false;
+  render:Boolean = true;
 
-  constructor(private categoriesService: CategoriesService) { }
+  constructor(private categoriesService: CategoriesService,
+    private subCategoriesService: SubCategoriesService) { }
 
   ngOnInit(): void {
 
@@ -49,4 +57,47 @@ export class HomeShowcaseComponent implements OnInit {
     })
   }
 
+  /*=============================================================
+  Función que nos avisa cuando finaliza el renderizado de Angular
+  =============================================================*/
+  callback(){
+    if(this.render){
+      this.render = false;
+
+      let arraySubCategories = [];
+
+      /*====================
+      Separar las categorías
+      ====================*/
+
+      this.categories.forEach(category=>{
+        /*================================================================================
+        Tomamos la colección de las sub-categorías filtrando con los nombres de categorías
+        ================================================================================*/
+        this.subCategoriesService.getFilterData("category",category.name)
+        .subscribe(resp=>{
+          let i;
+          for( i in resp){
+            arraySubCategories.push({
+              "category": resp[i].category,
+              "subcategory": resp[i].name,
+              "url": resp[i].url
+            })
+          }
+          /*====================================================================
+          Recorremos el array de objetos nuevo para buscar coincidencias con los
+          nombres de categorías
+          ====================================================================*/
+          for(i in arraySubCategories){
+            if(category.name == arraySubCategories[i].category){
+              $(`[category-showcase='${category.name}']`).append(`
+                <li><a href="products/${arraySubCategories[i].url}">${arraySubCategories[i].subcategory}</a></li>
+              `)
+            }
+          }
+        })
+      })
+
+    }
+  }
 }
